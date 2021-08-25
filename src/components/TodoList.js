f42cfd707ef60bb508d660aaa5816c7005e11682
MinJaeMoon <75596583+minjaemoon@users.noreply.github.com>
2021-08-25 23:10:59 +0900

    Add files via upload

---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import "./TodoList.css";
+
+const TodoList = ({ todos, onCheckToggle, onInsertToggle, onChangesetSelectedTodo }) => {
+    return (
+        <div className="TodoList">
+            {todos.map(todo => (
+                <TodoItem 
+                todo={todo} 
+                key={todo.id} 
+                onCheckToggle={onCheckToggle} 
+                onInsertToggle={onInsertToggle}
+                onChangesetSelectedTodo={onChangesetSelectedTodo}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
